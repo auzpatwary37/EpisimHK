1b561e0eb87935a234bc97b7cd979e594165c24c
Enoch <hqlx23@ust.hk>
2022-03-20 15:01:34 +0800

    Added age insertion and gv deletion

--- Episim/src/main/java/HKScenario/ReadAndChangeEventFile.java
+++ Episim/src/main/java/HKScenario/ReadAndChangeEventFile.java
@@ -20,7 +20,9 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.io.PopulationWriter;
@@ -43,19 +45,62 @@ public class ReadAndChangeEventFile{
 		String outEventFileIntermediate = "HKData/output/eventsHongKong_"+proportion;
 		String outputFile = outEventFileIntermediate+"/events_cleaned.xml";
 		String populationFileLoc = "HKData/output_plans.xml.gz";
+		String populationFileLoc1 = "HKData/output_plans_noGV_age.xml.gz";
 		String outputPopulationFileLocation = outEventFileIntermediate+"/output_plans_cleaned.xml.gz";
 		String googleMobilityDataRecord = "HKData/output/HKMobilityReport.csv";
+		String personAgeFile = "HKData/memberAge.csv";
 		
 		
+		//________________________Delete goods vehicle and insert age____________________________________________
+		Map<String,Double> ageOfPersons = new HashMap<>();
 		
+		try {
+			BufferedReader bf1 = new BufferedReader(new FileReader(new File(personAgeFile)));
+			
+			String line = null;
+			
+			while((line = bf1.readLine())!=null) {
+				String[] part = line.split(",");
+				ageOfPersons.put(part[0], Double.parseDouble(part[1]));
+			}
+			bf1.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		Population pp = PopulationUtils.readPopulation(populationFileLoc);
+		Map<Id<Person>, ? extends Person>personMap = pp.getPersons();
+		personMap.entrySet().forEach(e->{
+			if(PopulationUtils.getSubpopulation(e.getValue()).contains("GV")) {
+				pp.removePerson(e.getKey());
+				return;
+			}else {
+				String mId = e.getKey().toString().split("_")[0]+e.getKey().toString().split("_")[1];
+				if(ageOfPersons.get(mId)!=null) {
+					e.getValue().getAttributes().putAttribute("age",ageOfPersons.get(mId));
+				}else {
+					throw new IllegalArgumentException("No Age found for person Id = "+e.getKey().toString());
+				}
+			}
+			
+		});
+		
+		new PopulationWriter(pp).write(populationFileLoc1);
+		//____________cerate episim scenario(event file)______________________
 		String[] args1 = new String[]{
 				//DownSampleScenario.class.getName(),
 				Double.toString(proportion),
-				"--population", populationFileLoc,
+				"--population", populationFileLoc1,
 				"--events", inputFile,
 				"--output", outEventFileIntermediate
 		};
 		DownSampleScenario.main(args1);
+	
+		//________activity Type replacement______________________________________________________________
 
 		Map<String,String> actRepl = new HashMap<>();
 		try {
@@ -75,6 +120,7 @@ public class ReadAndChangeEventFile{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		// 1- Build the doc from the XML file
 		Document doc = DocumentBuilderFactory.newInstance()
 		            .newDocumentBuilder().parse(new InputSource(IOUtils.getBufferedReader(outEventFileIntermediate+"/output_events-"+proportion+".xml.gz")));
@@ -113,6 +159,7 @@ public class ReadAndChangeEventFile{
 		});
 		new PopulationWriter(pop).write(outputPopulationFileLocation);
 		
+		//____________________________google Mobility data download _______________________________________________
 		args1 = new String[]{
 				"--region", "HK",
 				"--sub-region","",
